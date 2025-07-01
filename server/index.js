abe2d670e304445a1b8a1a4627e436c1e9cda3e3
Suraj Shyam <102654234+regulus1128@users.noreply.github.com>
2025-07-01 20:02:57 +0530

    fixed cookie issues

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://reverie-weld.vercel.app"],
     credentials: true,
 }));
 
--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -4,11 +4,13 @@ export const generateToken = (userId, response) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d"
     })
+
+    const isDev = process.env.NODE_ENV === "development";
     response.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV != "development"
+        sameSite: isDev ? "Lax" : "None", 
+        secure: !isDev, 
     }) 
 
     return token;
