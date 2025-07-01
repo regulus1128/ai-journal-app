8b565bd212d22160b79b2f355aa3937ddb04de54
Suraj Shyam <102654234+regulus1128@users.noreply.github.com>
2025-07-01 19:52:24 +0530

    added backend url

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const URL = import.meta.env.VITE_BACKEND_URL;
+const URL = import.meta.env.MODE === "development"
+  ? "http://localhost:3000"
+  : import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
     baseURL: `${URL}/api`, // Base URL for the API
